b19229346a66d9e5912ca11577e411662971ca50
Polina <pkhrapunova@gmail.com>
2025-09-10 10:33:03 +0300

    Lab1

--- Lab1.1/Program.cs
+++ Lab1.1/Program.cs
@@ -1,15 +1,113 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab1._1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            try
+            {
+                Console.Write("Введите xn: ");
+                double xn = double.Parse(Console.ReadLine());
+
+                Console.Write("Введите xk: ");
+                double xk = double.Parse(Console.ReadLine());
+
+                Console.Write("Введите шаг h: ");
+                double h = double.Parse(Console.ReadLine());
+
+                Console.Write("Введите yn: ");
+                double yn = double.Parse(Console.ReadLine());
+
+                Console.Write("Введите yk: ");
+                double yk = double.Parse(Console.ReadLine());
+
+                Console.Write("Введите шаг t: ");
+                double t = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("\n{0,8}{1,8}{2,15}{3,15}", "x", "y", "f(x,y)", "g(x,y)");
+                Console.WriteLine(new string('-', 50));
+
+                for (double x = xn; x <= xk; x += h)
+                {
+                    for (double y = yn; y <= yk; y += t)
+                    {
+                        try
+                        {
+                            double fVal = F(x, y);
+                            double gVal = G(x, y);
+
+                            Console.WriteLine("{0,8:F2}{1,8:F2}{2,15:F5}{3,15:F5}", x, y, fVal, gVal);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("{0,8:F2}{1,8:F2}{2,15}{3,15}", x, y, "Ошибка", "Ошибка");
+                            Console.WriteLine("   -> Причина: " + ex.Message);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка ввода: " + ex.Message);
+            }
+        }
+
+        static double F(double x, double y)
+        {
+            double xy = x * y;
+
+            if (xy < 0)
+            {
+                double denominator = 1 + y * y - x;
+                if (denominator == 0)
+                    throw new DivideByZeroException("Знаменатель равен нулю в функции f.");
+
+                return (Math.Log(Math.Abs(xy)) + 1) / denominator;
+            }
+            else if (xy > 0)
+            {
+                double value = Math.Sin(x) + xy;
+                if (value < 0)
+                    throw new Exception("Подкоренное выражение < 0 в функции f.");
+
+                return Math.Sqrt(value);
+            }
+            else // xy == 0
+            {
+                double denominator = 1 - x * x + Math.Pow(y, 3);
+                if (denominator == 0)
+                    throw new DivideByZeroException("Знаменатель равен нулю в функции f.");
+
+                return (x - y) / denominator;
+            }
+        }
+
+        static double G(double x, double y)
+        {
+            double xy = x * y;
+
+            if (xy < 0)
+            {
+                double denominator = Math.Sqrt(2 * x * x + y);
+                if (denominator == 0)
+                    throw new DivideByZeroException("Знаменатель равен нулю в функции g.");
+
+                return (x * x - y * y + 3 * x) / denominator;
+            }
+            else if (xy > 0)
+            {
+                return x * x * x + y * y;
+            }
+            else // xy == 0
+            {
+                if (xy == 0)
+                    throw new Exception("Функция g не определена при ln(0).");
+
+                return (Math.Sin(x * x) + Math.Pow(y - x, 2)) / (25 + Math.Log(xy));
+            }
         }
     }
+
 }
---
+++ Lab1.2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Lab1.2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab1.2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("18bfa288-0188-4bb0-b7f8-1b320b12d36f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
